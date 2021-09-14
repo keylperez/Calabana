17e4d94576edcdff7cd4a119166dfc401827d92e
keylperez <perez.williamcaleb@gmail.com>
2021-09-14 21:12:37 +0800

    improvement in functionality

--- script.js
+++ script.js
@@ -11,26 +11,71 @@ const PRODUCTS=[
     {id:10,name:"Plates",category:["Utensil"]},
     {id:11,name:"Pork",category:["Meat"]}];
 
+//extracts categories from the PRODUCTS array of objects and removes duplicates
 const CATEGORIES = new Set(PRODUCTS.flatMap(el=>el.category));
 
-console.log(CATEGORIES);
+//containers
+const FILTLIST = document.querySelector('#filter-list');
+const TABLE = document.querySelector('.main-table');
 
-const FILTERLIST = document.querySelector('.filterList');
 
-CATEGORIES.forEach(el => {
-    FILTERLIST.innerHTML += `<input type="checkbox" name="filter" id=${el.toLowerCase()} value=${el.toLowerCase()} class="checkList"><label for=${el.toLowerCase()}>${el}</label><br>`
-    console.log();
+CATEGORIES.forEach(el => {//renders the filter names into html DOM
+    FILTLIST.innerHTML += `
+    <input type="checkbox" name="filter" id=${el.toLowerCase()} value=${el} class="check-list">
+    <label for=${el.toLowerCase()} class="check-list">${el}</label><br>`;
 });
 
+//renders the prodcuts and details
+PRODUCTS.forEach((el, i) => {
+    TABLE.insertAdjacentHTML('beforeend', `
+    <div class="table-row grid-container" id="${PRODUCTS[i].id}">
+    <h6 class="table-item">${PRODUCTS[i].id}</h6>
+    <h6 class="table-item">${PRODUCTS[i].name}</h6>
+    <h6 class="table-item">${PRODUCTS[i].category.join(', ')}</h6></div>`);
+});
 
+//all input type of checkbox
 const FILTERS = document.querySelectorAll('input[type="checkbox"]');
+const ROWS = document.querySelectorAll('.table-row');
+const ITEMS = document.querySelectorAll('.table-row');
 const FILTERED = [];
-console.log(FILTERS);
 
+// listens to each filter element when checked or unchecked and pushes or removes it into FILETERED array
 FILTERS.forEach(el => {
     el.addEventListener('change', () => {
+        for(let item of ITEMS) item.classList.add('no-display');
         if(el.checked) FILTERED.push(el.value);
-        if(!el.checked) FILTERED.pop(el.value);
+        if(!el.checked) FILTERED.splice(FILTERED.indexOf(el.value), 1);
         console.log(FILTERED);
+        displayFilter();
+        undisplayFilter();
+
     });
 });
+
+//function to filter display
+function displayFilter(){
+    FILTERED.forEach((filter, index) => {
+        
+        for(let product of PRODUCTS){
+            // console.log(`filter: ${filter}; categ1 = ${categ1}; categ2 = ${categ2}`);
+            
+            let categ1 = product.category[0];
+            let categ2 = product.category[1];
+            let item = ITEMS[product.id-1];
+
+            if((filter === categ1 && filter != categ2) ||
+            (filter != categ1 && filter === categ2)) {
+                item.classList.remove('no-display');
+            }
+
+            console.log(`filter: ${filter}\ncateg1: ${categ1}\ncateg2: ${categ2}\n`);
+            console.log(item);
+        }
+    });
+}
+
+function undisplayFilter(){
+    for(let i = 0; i < FILTERS.length && FILTERS[i].checked === true; i++){}
+    
+}
